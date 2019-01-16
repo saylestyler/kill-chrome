c304636d90519b4eb24f217fbcbdeb59a9aaf135
saylestyler <saylestyler@gmail.com>
2019-01-16 17:19:26 -0500

    remove ! before ext processes :-)

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-'use strict';
-const fkill = require('fkill');
-let psList = require('ps-list');
+'use strict'
+const fkill = require('fkill')
+let psList = require('ps-list')
 
 if (process.platform === 'win32') {
-	psList = require('./win');
+  psList = require('./win')
 }
 
 module.exports = async (options = {}) => {
-	const list = await psList();
+  const list = await psList()
 
-	const processes = {
-		chrome: process.platform === 'darwin' ? 'Chrome Helper' : 'chrome',
-		chromium: process.platform === 'darwin' ? 'Chromium Helper' : 'chromium'
-	};
+  const processes = {
+    chrome: process.platform === 'darwin' ? 'Chrome Helper' : 'chrome',
+    chromium: process.platform === 'darwin' ? 'Chromium Helper' : 'chromium'
+  }
 
-	if (options.chromium === false) {
-		delete processes.chromium;
-	}
+  if (options.chromium === false) {
+    delete processes.chromium
+  }
 
-	if (options.chrome === false) {
-		delete processes.chrome;
-	}
+  if (options.chrome === false) {
+    delete processes.chrome
+  }
 
-	const pids = list
-		.filter(x =>
-			Object.keys(processes).some(name => x.cmd.includes(processes[name])) &&
-			x.cmd.includes('--type=renderer') &&
-			!x.cmd.includes('--extension-process')
-		)
-		.map(x => x.pid);
+  const pids = list
+    .filter(x => Object.keys(processes).some(name => x.cmd.includes(processes[name])) && x.cmd.includes('--type=renderer') && x.cmd.includes('--extension-process'))
+    .map(x => x.pid)
 
-	return fkill(pids, {force: true});
-};
+  console.log(pids)
+
+  return fkill(pids, { force: true })
+}
